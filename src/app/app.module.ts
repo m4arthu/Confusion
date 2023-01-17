058605c97cb49a1a48cb92f374c75a522032cf65
Luis Arthur <luisarthir259@gmail.com>
2023-01-17 12:06:56 -0300

    angular components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from   '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout' /** this module meke abble the angular to  organize the layout**/
 import { AppComponent } from './app.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
