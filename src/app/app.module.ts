07ac9494a680a82b4d4c7ad760f8b6c1e6a16dac
Luis Arthur <luisarthir259@gmail.com>
2023-01-26 17:18:08 -0300

    SPA  confusion pt 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../app/app-routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from   '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout' /** this module meke abble the angular to  organize the layout**/
@@ -8,15 +8,31 @@ import { AppComponent } from './app.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+// components imports 
 
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component'
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './menu/dishdetail/dishdetail.component'
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,  
   ],
   imports: [
     BrowserModule,
@@ -26,9 +42,12 @@ import { MenuComponent } from './menu/menu.component'
     BrowserAnimationsModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [ 
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
